80ce05e24efda6b44d3f4527029e7d48ae31cf29
maxiamikel19 <maxloversist19@gmail.com>
2025-06-20 06:59:43 -0400

    First commit

---
+++ src/test/java/com/maxiamikel/userAuthApi/UserAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxiamikel.userAuthApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
