95489c8d15c40a43963e5a2623cc7e2a4c1a873a
maxiamikel19 <maxloversist19@gmail.com>
2025-06-20 07:53:02 -0400

    Configurar spring security e jwt

---
+++ src/main/java/com/maxiamikel/userAuthApi/entity/Role.java
@@ -0,0 +1,34 @@
+package com.maxiamikel.userAuthApi.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
